8bdd1126b848dc5ab0d8cf19f719049494463989
kabinny <kabinny@gmail.com>
2023-12-17 16:11:42 +0900

    홈 컴포넌트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,39 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">Post List</div>
+      <div className="post__navigation">
+        <div className="post__navigation--active">전체</div>
+        <div>나의 글</div>
+      </div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">작가 이름</div>
+                <div className="post__date">2023.12.17 일요일</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut
+                animi accusantium assumenda, nemo laudantium placeat
+                voluptatibus sit exercitationem amet dolore possimus ab
+                expedita, minima quis aspernatur iusto reprehenderit! Porro,
+                ipsam.
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </div>
   )
