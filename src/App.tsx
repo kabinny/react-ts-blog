de231d3a426a73aefbe8aed81883d3be09cdbc78
kabinny <kabinny@gmail.com>
2023-12-17 23:07:20 +0900

    Firebase Auth로 회원가입 기능

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import { app } from 'firebaseApp'
 import { getAuth } from 'firebase/auth'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Router from 'components/Router'
 
 function App() {
   const auth = getAuth(app)
-  console.log(auth)
+
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   )
 
-  return <Router isAuthenticated={isAuthenticated} />
+  return (
+    <>
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
+    </>
+  )
 }
 
 export default App
