38762383ca95f3dbde46b53a763dc368c5e9e314
kabinny <kabinny@gmail.com>
2023-12-27 21:26:32 +0900

    Firestore 데이터 삭제

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,8 +1,9 @@
 import AuthContext from 'context/AuthContext'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore'
 import { db } from 'firebaseApp'
 import { useContext, useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 interface PostListProps {
   hasNavigation?: boolean
@@ -29,10 +30,22 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
   const getPosts = async () => {
     const data = await getDocs(collection(db, 'posts'))
 
+    setPosts([])
+
     data.forEach((doc) => {
       const dataObj = { ...doc.data(), id: doc.id }
       setPosts((prev) => [...prev, dataObj as PostProps])
     })
+  }
+
+  const handleDelete = async (id: string) => {
+    const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?')
+    if (confirm && id) {
+      await deleteDoc(doc(db, 'posts', id))
+      toast.success('게시글을 삭제했습니다.')
+
+      getPosts()
+    }
   }
 
   useEffect(() => {
@@ -75,7 +88,13 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
               </Link>
               {post?.email === user?.email && (
                 <div className="post__utils-box">
-                  <div className="post__delete">삭제</div>
+                  <div
+                    className="post__delete"
+                    role="presentation"
+                    onClick={() => handleDelete(post.id as string)}
+                  >
+                    삭제
+                  </div>
                   <div className="post__edit">
                     <Link to={`/posts/edit/${post?.id}`}>수정</Link>
                   </div>
